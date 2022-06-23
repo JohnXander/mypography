e26f362cc5adfcb666b91e5e6a9f1329356625fb
John Xander <johnblox5@gmail.com>
2022-06-23 21:04:36 +0200

    cleaner code

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
-
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-const ctx = canvas.getContext('2d');
-
-const submitBtn = document.getElementById("submitBtn")
-
-const img1 = document.getElementById("img1")
-const img2 = document.getElementById("img2")
-const img3 = document.getElementById("img3")
-const img4 = document.getElementById("img4")
-const img5 = document.getElementById("img5")
-const img6 = document.getElementById("img6")
-const img7 = document.getElementById("img7")
-const img8 = document.getElementById("img8")
-const img9 = document.getElementById("img9")
-const img10 = document.getElementById("img10")
-const img11 = document.getElementById("img11")
-const img12 = document.getElementById("img12")
-const img13 = document.getElementById("img13")
-const img14 = document.getElementById("img14")
-const img15 = document.getElementById("img15")
-const img16 = document.getElementById("img16")
-const img17 = document.getElementById("img17")
-const img18 = document.getElementById("img18")
-const img19 = document.getElementById("img19")
-const img20 = document.getElementById("img20")
-const img21 = document.getElementById("img21")
-const img22 = document.getElementById("img22")
-const img23 = document.getElementById("img23")
-const img24 = document.getElementById("img24")
-const img25 = document.getElementById("img25")
-const img26 = document.getElementById("img26")
-const img27 = document.getElementById("img27")
-const img28 = document.getElementById("img28")
-const img29 = document.getElementById("img29")
-const img30 = document.getElementById("img30")
-const img31 = document.getElementById("img31")
-const img32 = document.getElementById("img32")
-const img33 = document.getElementById("img33")
-const img34 = document.getElementById("img34")
-const img35 = document.getElementById("img35")
-
-const row1 = [img1, img2, img3, img4, img5, img6, img7, img8]
-const row2 = [img9, img10, img11, img12, img13, img14, img15, img16]
-const row3 = [img17, img18, img19, img20, img21, img22, img23, img24, img25]
-const row4 = [img26, img27, img28, img29, img30, img31, img32, img33, img34, img35]
-
-const imgArr = row1.concat(row2).concat(row3).concat(row4)
-
-let isPressed = false;
+let btnIsPressed = false;
 let x = undefined;
 let y = undefined;
 
-canvas.addEventListener("mousedown", (e) => {
-    isPressed = true;
-    x = e.offsetX;
-    y = e.offsetY;
+canvas.addEventListener("mousedown", (event) => {
+    drawOnCanvas("mousedown", event)
 });
 
-canvas.addEventListener("mouseup", (e) => {
-    isPressed = false;
-    x = undefined;
-    y = undefined;
+canvas.addEventListener("mouseup", () => {
+    drawOnCanvas("mouseup")
 })
 
+const drawOnCanvas = (mouseAction, event) => {
+    switch (mouseAction) {
+        case "mousedown":
+            btnIsPressed = true;
+            x = event.offsetX;
+            y = event.offsetY;
+            break;
+        case "mouseup":
+            btnIsPressed = false;
+            x = undefined;
+            y = undefined;
+            break;
+    }
+}
+
 canvas.addEventListener("mousemove", (e) => {
-    if (isPressed) {
+    if (btnIsPressed) {
         const x2 = e.offsetX;
         const y2 = e.offsetY;
     
@@ -77,28 +41,57 @@ canvas.addEventListener("mousemove", (e) => {
 });
 
 const drawCircle = (x, y) => {
-    ctx.beginPath();
-    ctx.arc(x, y, 8, 0, Math.PI * 2);
-    ctx.fillStyle = "black";
-    ctx.fill();
+    context.beginPath();
+    context.arc(x, y, 8, 0, Math.PI * 2);
+    context.fillStyle = "black";
+    context.fill();
 };
 
 const drawLine = (x1, y1, x2, y2) => {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 8 * 2;
-    ctx.stroke();
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.strokeStyle = "black";
+    context.lineWidth = 8 * 2;
+    context.stroke();
 };
+
+const submitBtn = document.getElementById("submitBtn")
+
+const container1 = document.getElementById("container1")
+const container2 = document.getElementById("container2")
+const container3 = document.getElementById("container3")
+const container4 = document.getElementById("container4")
 
 let num = 0
 
 submitBtn.addEventListener("click", () => {
+    printLetterToBoard()
+})
+
+const printLetterToBoard = () => {
+    const iterableArr = Array(35).fill(0)
     const dataURI = canvas.toDataURL()
     num++
-    imgArr.map((item, idx) => {
-        if (num === idx + 1) item.src = dataURI
+    iterableArr.map((_, idx) => {
+        if (num === idx + 1) {
+            const img = document.createElement("img")
+            const space = document.createElement("span")
+            space.innerHTML = "___"
+            img.src = dataURI
+            img.id = `img${idx + 1}`
+
+            if (num === 4) container1.appendChild(space)
+            if (num === 14) container2.appendChild(space)
+            if (num === 22) container3.appendChild(space)
+            if (num === 29 || num === 33) container4.appendChild(space)
+
+            if (num <= 8) container1.appendChild(img)
+            if (num > 8 && num <= 16) container2.appendChild(img)
+            if (num > 16 && num <= 25) container3.appendChild(img)
+            if (num > 25) container4.appendChild(img)
+        }
+        
     })
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-})
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
